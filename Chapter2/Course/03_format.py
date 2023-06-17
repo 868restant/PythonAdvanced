a868dacb054f8784f3696c3d68c604304f57775f
868restant <huangclement5@gmail.com>
2023-06-17 17:06:50 +0200

    first course

---
+++ Chapter2/Course/03_format.py
@@ -0,0 +1,6 @@
+num1 = 50
+num2 = 50.897
+print("Digital 50 limite de largeur 5, résultat:%5d" % num1)
+print("Digital 50 limite de largeur 1, résultat:%1d" % num1)
+print("chiffre 50.897 limite de largeur 7, précision décimal 2, résultat:%7.2f" % num2)
+print("chiffre 50.897 limite illimitée, précision décimale 2, résultat: %.2f" % num2)
