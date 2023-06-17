c5c650cc92dae01219a06210bfde32d7ff097137
868restant <huangclement5@gmail.com>
2023-06-17 17:06:25 +0200

    first course

--- Chapter2/Course/02_string.py
+++ Chapter2/Course/02_string.py
@@ -1,3 +1,13 @@
 name = 'alex'
 address = 'paris'
-print("je m'appelle " + name + " et j'habite à " + address)
+age = 18
+height = 1.83
+print("je m'appelle " + name + " j'habite à " + address + " et j'ai " + str(age) + 'ans')
+print("je m'appelle %s . j'habite à %s et j'ai %d ans . " % (name,address,age))
+print("je m'appelle %s.j'ai %d ans et je mesure %.2f m." % (name,age,height))
+
+print(f"je m'appelle {name}, j'ai {age} ans et je mesure {height} m.")
+
+print("le résultat de 1 + 1 est: %d" % (1*1))
+print(f"le résultat de 1 + 1 est:{1*1}")
+print("le type de chaine en Python est: %s" %type('chaine'))
