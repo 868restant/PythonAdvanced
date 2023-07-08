6a4ca290bf8345e7ecfbfa28833ada126f6dcb3d
868restant <huangclement5@gmail.com>
2023-07-08 15:12:50 +0200

    course

--- chapter3/course/05_number.py
+++ chapter3/course/05_number.py
@@ -1,8 +1,25 @@
 num = 5
 
 number = int(input("veuillez devinez un numero :"))
-if number >= num:
+if number == num:
     print("felicitation")
 else:
-    print("mauvaise reponse encore une fois ")
+    number = int(input("veuillez devinez un numero :"))
+    if number % num:
+        print("mauvaise reponse encore une fois ")
+else:
+    print("felicitation pour votre bonne reponse")
+
+    number = int(input("veuillez devinez un numero :"))
+    if number % num:
+        print("mauvaise reponse encore une fois")
+
+    number = int(input("veuillez devinez un nombre "))
+    if number % num:
+        print("mauvaise reponse encore une fois")
+else:
+    print("vous avez mal devine")
+print("mauvaise reponse encore une fois ")
+
+
 
