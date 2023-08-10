dab97809f3ec8b26103dfc8a34414b70b89045ea
868restant <huangclement5@gmail.com>
2023-08-10 11:08:33 +0200

    course

---
+++ Chapter4/Course/08_even.py
@@ -0,0 +1,12 @@
+num = 0
+for number in range(1,100):
+    if number % 2 == 0:
+        num = num + 1
+print(str(num) + " nombres pairs dans l'intervalle de 1 a 100(a l'exception de 100 lui-meme")
+"""
+i = 0
+for i in range(5):
+    print(i)
+    
+print(i)
+"""
