33c26041db7c14a7704312eee1e26ea118a87096
868restant <huangclement5@gmail.com>
2023-08-12 15:29:39 +0200

    course

--- Chapter4/project/table_for.py
+++ Chapter4/project/table_for.py
@@ -1,4 +1,4 @@
-for a in range(1,11):
-    print("voici la table de ", a)
-    for i in range(1,10):
-        print(i,"*",a,"=",i*a)
+for a in range(1,10):
+    for i in range(1, a + 1):
+        print(str(i) + " * " + str(a) + " = " + str(i*a), end="\t")
+    print()
